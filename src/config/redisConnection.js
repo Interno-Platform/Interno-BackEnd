c95c821a6d66b1cdfcc86247b1cf4f19abfdb393
Moamen 1589 <mwmnadl11@gmail.com>
2026-04-24 04:40:52 +0300

    refactor: update Redis client configuration to use environment variable for URL

--- src/config/redisConnection.js
+++ src/config/redisConnection.js
@@ -1,9 +1,6 @@
 const redis = require("redis");
 const redisClient = redis.createClient({
-  socket: {
-    host: "127.0.0.1",
-    port: 6379,
-  },
+  url: process.env.REDIS_URL,
 });
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 (async () => {
